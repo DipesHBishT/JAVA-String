54cd674732d5ca9c71b012dd20f37a0eff58120b
Dipesh Bisht <bisht87dipesh1@gmail.com>
2018-05-23 23:39:31 +0530

    First commit

---
+++ src/stringfun/StringFun.java
@@ -0,0 +1,22 @@
+
+package stringfun;
+
+/**
+ *
+ * @author dipesh bIsht
+ */
+public class StringFun {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String s= new String("DIPESH");
+        String s1=new String("dipesh");
+        
+        System.out.println("Using Compare function:"+s.compareTo(s1));
+        System.out.println("Using ")
+        
+    }
+    
+}
